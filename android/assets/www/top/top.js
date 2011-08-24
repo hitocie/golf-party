cd74582cc6d50dd6d225de044c39603d4d576874
yuarai <yuki.arai.jp@gmail.com>
2011-08-24 13:06:34 +0900

    test

---
+++ android/assets/www/top/top.js
@@ -0,0 +1,15 @@
+
+/* Event handler */
+$(function() {
+	$("#popup1").click(function() {
+		alert('未実装');
+	});
+});
+
+
+/* Event handler */
+$(function() {
+	$("#popup2").click(function() {
+		alert('未実装');
+	});
+});
