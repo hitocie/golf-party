9350f754b3540da0664e37b36aa0d8504738e00c
hokada <hitoshi.dasu@gmail.com>
2011-11-02 00:45:00 +0900

    Fixed headers.

--- server/war/group/create.js
+++ server/war/group/create.js
@@ -9,15 +9,13 @@ $('#group-add').live('pagehide', function(event, ui) {
 
 
 
-$(function() {
-	$('#added').click(function() {
-		var new_group = $('#group-name').val()
-		if (new_group != '') {
-			set_storage('new-group', new_group);
-		}
-		$.mobile.changePage(
-				'list.html', 'pop', false, false
-		);
-	});	
-});
+$('#added').click(function() {
+	var new_group = $('#group-name').val()
+	if (new_group != '') {
+		set_storage('new-group', new_group);
+	}
+	$.mobile.changePage(
+			'list.html', 'pop', false, false
+	);
+});	
 
--- server/war/group/edit.js
+++ server/war/group/edit.js
@@ -5,19 +5,21 @@ $('#group-edit').live('pageshow', function(event, ui) {
 	var lv = $('#members');
 	var prev_page = ui.prevPage[0].id;
 	if (prev_page == 'group-list') {
-		console.log('Choice id=' + v.id + ' name=' + v.name);
 		lv.empty();
 		var users = JSON.parse(v.users);
 		for (var i = 0 in users) {
 			var u = users[i];
-			lv.append('<li>' + u.name + '</li>');
+			lv.append('<li><a href="#">' + u.name + '</a><a id="delete-user" href="#">' + u.id + '</a></li>');
 		}
 		lv.listview('refresh');
 		set_storage('group', {id: v.id, name: v.name, users: users});
 
 	} else if (prev_page == 'friend-list') {
 		var group = get_storage('group');
-		group.users.push({id: v.id, name: v.name});
+		// not back button
+		if (v.id != undefined) {
+			group.users.push({id: v.id, name: v.name});
+		}
 		for (var i = 0 in group.users) {
 			var u = group.users[i];
 			lv.append('<li>' + u.name + '</li>');
@@ -27,8 +29,15 @@ $('#group-edit').live('pageshow', function(event, ui) {
 	}
 });
 
+$('#delete-user').live('click', function() {
+	// TODO: val() ? text() ?
+	console.log($('#delete-user'));
+});
+
+
 $('#group-edit').live('pagehide', function(event, ui) {
 	$('#group-edit').die('pageshow');
+	$('#delete-user').die('click');
 	if (ui.nextPage[0].id == 'group-list') {
 		remove_storage('group');
 	}
@@ -36,16 +45,12 @@ $('#group-edit').live('pagehide', function(event, ui) {
 
 
 
-$(function() {
-	$('#save').click(function() {
-		var g = get_storage('group');
-		var userids = [];
-		for (var i in g.users) {
-			var u = g.users[i];
-			userids.push(u.id);
-		}
-		update_group({id: g.id, name: g.name, userids: userids});
-	});	
+$('#save').click(function() {
+	var g = get_storage('group');
+	var userids = [];
+	for (var i in g.users) {
+		var u = g.users[i];
+		userids.push(u.id);
+	}
+	update_group({id: g.id, name: g.name, userids: userids});
 });
-
-
