ca4b8e9fa325f6f62f91b411b03440564f947824
yuarai <yuki.arai.jp@gmail.com>
2011-11-02 14:47:31 +0900

    edit

--- server/war/play/golf-input.js
+++ server/war/play/golf-input.js
@@ -1,4 +1,30 @@
+var golf_info;
+var me;
 
+function parse_query(q) {
+    var exps = q.substr(1).split('&');
+    var results = [];
+    for (var i = 0 in exps) {
+	var p = exps[i].split('=');
+	results[p[0]] = p[1];
+    }
+    return results;
+}
+
+
+$('#golf-input').live('pageshow', function(event, ui) {
+	var v = parse_query(location.search);
+	var jo = JSON.parse(decodeURI(v['j']));
+	golf_info = jo;
+	get_me(function(user) {
+		me = user; // set user to global variable.
+		console.log(me.id);
+	});
+
+	var golf_name_l = $('#golf-name-l')
+	golf_name_l.text(golf_info.name);
+	console.log(golf_info.name);
+});
 
 /* Event handler */
 $(function() {
@@ -6,3 +32,29 @@ $(function() {
 
 	});
 });
+
+/* Event handler */
+$(function() {
+	$("#play").click(function() {
+		opnClz();
+	});
+});
+
+function opnClz(){
+	
+
+	var round = {
+			course: golf_info.name,
+			date: '2011-10-10 09:00:00',
+			first_half: 'OUT',
+			last_half: 'IN',
+			weather: Weather.fine,
+			wind: Wind.no,
+			groups: [
+			         [{id: me.id, scores: null}]
+			         ]
+	};
+	console.log(round.course);
+	round = create_round(round);
+	alert('登録');
+};
--- server/war/play/golf-select.js
+++ server/war/play/golf-select.js
@@ -1,4 +1,27 @@
-﻿
+﻿function load()
+{
+	alert('未実装');
+}
+
+$('#golf-select').live('pageshow', function(event, ui) {
+	
+	get_all_courses(function(courses){
+		var golf_list = $('#golf-list');
+		golf_list.empty();
+		for (var i in courses) {
+			/*追加処理*/
+			var c = courses[i];
+			var str = encodeURI(JSON.stringify(courses[i]));
+			golf_list.append('<li><a href="golf-input.html?p=' + c.name + '&j=' + str + '">' + c.name + '</a></li>');
+		}
+		golf_list.append('<li><a href="golf-input.html?p=' + '"ダミー"' + '>ダミー</a></li>');
+		golf_list.listview('refresh');
+	}); 
+	
+	
+});
+
+
 /* Event handler */
 $(function() {
 	$("#popup1").click(function() {
